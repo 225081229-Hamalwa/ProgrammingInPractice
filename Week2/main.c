a20e7d216f98afd38323dd8ff5ed33ede110b13f
225081229-Hamalwa <225081229@nust.na>
2026-08-28 21:45:27 +0200

    Add Week 2 - Budget Calculator

---
+++ Week2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    double revenue;
+    double expenses;
+    double financialBalance;
+
+    printf("Please Enter Revenue: \n");
+    scanf("%lf", &revenue);
+
+    printf("Please Enter Expenses: \n");
+    scanf("%lf", &expenses);
+
+    financialBalance = revenue - expenses;
+
+    if (revenue > expenses) {
+        printf("There is a surplus.\n");
+        printf("The Balance is: N$ %.2f\n", financialBalance);
+
+    } 
+    else if (expenses > revenue) {
+        printf("There is a deficit.\n");
+        printf("The Balance is: N$ %.2f\n", -financialBalance);
+
+    } 
+    else {
+        printf("Balance is balanced!\n");
+        printf("Balance is: N$ %.2f\n", financialBalance);
+    }
+
+    return 0;
+}
